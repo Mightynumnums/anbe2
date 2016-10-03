5ca79a359697706f0807b18b544302aab5e73554
aleksandra shineleva <deadcookies@gmail.com>
2016-10-02 23:02:10 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
 
-	$(function(){
-		$('#menu').slicknav();
-	});
+$(function(){
+	$('#menu').slicknav();
+});
 
-
-	    $(document).ready(function(){
-	    	var parameters = {
-				dots: false,
-				arrows: false,
-				swipeToSlide: true,
-				autoplay: true,
-				autoPlayTimer: 3000,
-				infinite: true,
-				speed: 500,
-				fade: true,
-				mobileFirst: true,
-				cssEase: 'linear'
-			}
-
-	      	$('.fade').slick(parameters);
-	    });
 
 
 
