72def5da869f5ab66f9c79285db5e793757fdd9c
aleksandra shineleva <deadcookies@gmail.com>
2016-10-02 22:43:31 -0400

    slick nav added, lightbox fixed

---
+++ app.js
@@ -0,0 +1,26 @@
+
+	$(function(){
+		$('#menu').slicknav();
+	});
+
+
+	    $(document).ready(function(){
+	    	var parameters = {
+				dots: false,
+				arrows: false,
+				swipeToSlide: true,
+				autoplay: true,
+				autoPlayTimer: 3000,
+				infinite: true,
+				speed: 500,
+				fade: true,
+				mobileFirst: true,
+				cssEase: 'linear'
+			}
+
+	      	$('.fade').slick(parameters);
+	    });
+
+
+
+
